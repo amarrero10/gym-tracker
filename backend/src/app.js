41d9d61124d606424109f477bd8bace796b9414c
Albert Marrero <albert.marrero10@gmail.com>
2026-03-10 11:07:45 -0400

    Fix backend issue

--- backend/src/app.js
+++ backend/src/app.js
@@ -28,7 +28,7 @@ app.use("/api/auth", authRouter);
 if (process.env.NODE_ENV === "production") {
   const frontendDist = path.resolve(__dirname, "../../frontend/dist");
   app.use(express.static(frontendDist));
-  app.get("*", (req, res) => {
+  app.get("/{*splat}", (req, res) => {
     res.sendFile(path.join(frontendDist, "index.html"));
   });
 }
